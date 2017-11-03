e1cdc32605c6ebb661520eb4dd3565666ecbdeb8
LAPTOP-2KGNLENP\Thoma <Thomaslienchristensen@gmail.com>
2017-11-03 16:10:47 +0100

    Changed fragment_weapon_layout to fragment_weapon_details_layout

--- app/src/main/java/dk/blackdarkness/g17/cphindustries/entityfragments/WeaponFragment.java
+++ app/src/main/java/dk/blackdarkness/g17/cphindustries/entityfragments/WeaponFragment.java
@@ -27,7 +27,7 @@ public class WeaponFragment extends Fragment implements View.OnClickListener {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_weapon_layout, container, false);
+        View view = inflater.inflate(R.layout.fragment_weapon_details_layout, container, false);
         dummy = view.findViewById(R.id.weaponDummy);
         lock = view.findViewById(R.id.lockFab);
         initDisplay();
