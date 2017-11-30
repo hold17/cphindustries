3d0cc7bc585e7c48f3d132a95bc47402caca285c
Anders Wiberg Olsen <anders@wiberg.tech>
2017-11-30 23:01:20 +0100

    Cleanup

--- app/src/main/java/dk/blackdarkness/g17/cphindustries/createfragments/CreateWeaponFragment.java
+++ app/src/main/java/dk/blackdarkness/g17/cphindustries/createfragments/CreateWeaponFragment.java
@@ -1,27 +1,25 @@
 package dk.blackdarkness.g17.cphindustries.createfragments;
 
-        import android.app.ProgressDialog;
-        import android.os.AsyncTask;
-        import android.os.Bundle;
-        import android.support.annotation.Nullable;
-        import android.support.constraint.ConstraintLayout;
-        import android.support.v4.app.Fragment;
-        import android.util.Log;
-        import android.view.LayoutInflater;
-        import android.view.View;
-        import android.view.ViewGroup;
-        import android.widget.RadioButton;
-        import android.widget.RadioGroup;
-        import android.widget.TextView;
-        import java.util.ArrayList;
-        import dk.blackdarkness.g17.cphindustries.R;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.constraint.ConstraintLayout;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import java.util.ArrayList;
+import dk.blackdarkness.g17.cphindustries.R;
 
 /**
  * Created by Thoma on 11/03/2017.
  */
 
 public class CreateWeaponFragment extends Fragment implements View.OnClickListener {
-
     private static final String TAG = "CreateWeaponFragment";
     private View view;
     private TextView submitSave, submitCancel;
@@ -32,12 +30,16 @@ public class CreateWeaponFragment extends Fragment implements View.OnClickListen
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_create_weapon_layout, container, false);
+
         submitSave = view.findViewById(R.id.fr_create_weapon_tvSave);
         submitCancel = view.findViewById(R.id.fr_create_weapon_tvCancel);
+
         rgLeft = view.findViewById(R.id.fr_create_weapon_radioGroup_left);
         rgRight = view.findViewById(R.id.fr_create_weapon_radioGroup_right);
+
         radioButtons = view.findViewById(R.id.fr_create_weapon_constraintLayout_bottom);
         loading = view.findViewById(R.id.fr_create_weapon_constraintLayout_loading);
+
         initLayout();
         Log.d(TAG, "onCreateView: Returning");
         return view;
@@ -45,19 +47,23 @@ public class CreateWeaponFragment extends Fragment implements View.OnClickListen
 
     public void initLayout() {
         getActivity().setTitle("Create Weapon");
+
         radioButtons.setVisibility(View.GONE);
+
         submitSave.setOnClickListener(this);
         submitSave.setText("Submit");
+
         submitCancel.setOnClickListener(this);
         submitCancel.setText("Cancel");
+
         rgLeft.setOnCheckedChangeListener(lis1);
         rgRight.setOnCheckedChangeListener(lis2);
+
         new DeviceLocation().execute();
     }
 
     @Override
     public void onClick(View view) {
-
         switch (view.getId()) {
             case R.id.fr_create_weapon_tvSave:
                 getActivity().onBackPressed();
@@ -69,7 +75,6 @@ public class CreateWeaponFragment extends Fragment implements View.OnClickListen
     }
 
     private RadioGroup.OnCheckedChangeListener lis1 = new RadioGroup.OnCheckedChangeListener() {
-
         @Override
         public void onCheckedChanged(RadioGroup group, int checkedId) {
             if (checkedId != -1) {
@@ -81,7 +86,6 @@ public class CreateWeaponFragment extends Fragment implements View.OnClickListen
     };
 
     private RadioGroup.OnCheckedChangeListener lis2 = new RadioGroup.OnCheckedChangeListener() {
-
         @Override
         public void onCheckedChanged(RadioGroup group, int checkedId) {
             if (checkedId != -1) {
@@ -100,8 +104,8 @@ public class CreateWeaponFragment extends Fragment implements View.OnClickListen
         protected String doInBackground(String... params) {
 
             try {
-                //fetch ip / mac addresses from nearby devices
-                //Curently dummy data - use actual weapon DTO
+                // Fetch ip / mac addresses from nearby devices
+                // Currently dummy data - use actual weapon DTO
                 this.weaponIpList = new ArrayList<>();
                 this.weaponIpList.add("123.123.123.123");
                 this.weaponIpList.add("234.234.234.234");
@@ -115,7 +119,7 @@ public class CreateWeaponFragment extends Fragment implements View.OnClickListen
 
         @Override
         protected void onPostExecute(String result) {
-            //setup radio button view with found weapons
+            // Setup radio button view with found weapons
             int rgHalf = (weaponIpList.size()) / 2;
             final RadioButton[] rb = new RadioButton[weaponIpList.size()];
             for (int i = 0; i < weaponIpList.size(); i++) {
