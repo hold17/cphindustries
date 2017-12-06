44348c3edbbe5bcaf744e8a1c182dcb1976cefe7
awo-dev <anders@wiberg.tech>
2017-12-06 14:39:36 +0100

    Application configuration

---
+++ app/src/main/java/dk/blackdarkness/g17/cphindustries/dataaccess/ApplicationConfig.java
@@ -0,0 +1,22 @@
+package dk.blackdarkness.g17.cphindustries.dataaccess;
+
+import android.app.Application;
+
+/**
+ * Created by awo on 06-12-2017.
+ */
+
+public class ApplicationConfig extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    /**
+     * Decides whether or not hardcoded data is used or not
+     * @return true if demo data is used, false if data should be fetched from api or cache
+     */
+    public static boolean useDemoData() {
+        return true; // TODO: Read from shared prefs or something...
+    }
+}
