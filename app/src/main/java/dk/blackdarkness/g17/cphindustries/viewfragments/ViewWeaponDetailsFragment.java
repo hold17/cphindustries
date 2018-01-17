770e9700a5ce3e392b103876407949ffa9ebcf16
LAPTOP-2KGNLENP\Thoma <Thomaslienchristensen@gmail.com>
2018-01-17 23:43:58 +0100

    Minor changes

--- app/src/main/java/dk/blackdarkness/g17/cphindustries/viewfragments/ViewWeaponDetailsFragment.java
+++ app/src/main/java/dk/blackdarkness/g17/cphindustries/viewfragments/ViewWeaponDetailsFragment.java
@@ -31,7 +31,7 @@ import static dk.blackdarkness.g17.cphindustries.dto.FireMode.SINGLE;
 
 public class ViewWeaponDetailsFragment extends Fragment implements View.OnClickListener {
     private View view;
-    private static final String TAG = "ViewWeaponDetailsFragment";
+    private static final String TAG = "ViewWeaponDetailsFrag";
     private TextView weaponIpText, weaponMacText, weaponImageDescription,
             weaponNameText, weaponIdText, weaponFiremodeText, weaponShootsText, statusText;
     private FloatingActionButton lock;
@@ -99,10 +99,16 @@ public class ViewWeaponDetailsFragment extends Fragment implements View.OnClickL
         StringBuilder sb = new StringBuilder();
         int counter = 0;
         for (Shoot s : this.weaponDao.getShootsByWeapon(this.weapon.getId())) {
+            if(counter == 3) {
+                sb.append("...");
+                break;
+            }
             sb.append(s.getName());
             counter++;
-            if(counter != this.weaponDao.getShootsByWeapon(this.weapon.getId()).size())
-            sb.append(", ")/*.append("\n")*/;
+            if(counter != this.weaponDao.getShootsByWeapon(this.weapon.getId()).size()) {
+                sb.append(", ")/*.append("\n")*/;
+            }
+
         }
         this.weaponShootsText.setText(sb);
 
